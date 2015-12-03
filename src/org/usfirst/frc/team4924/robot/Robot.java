f7c317136280e64fd7e9864980bbeeba1d3c431a
Elias Basler <e.e.basler@gmail.com>
2015-12-03 17:44:37 -0500

    auto class update

--- src/org/usfirst/frc/team4924/robot/Robot.java
+++ src/org/usfirst/frc/team4924/robot/Robot.java
@@ -29,7 +29,9 @@ public class Robot extends IterativeRobot {
     Relay relayIn;
     Compressor pump;
 	int autoLoopCounter;
-	Servo servo; 
+	Servo servo;
+	commandObject onecommand;
+	commandObject[] autoArray;
 	
     /**
      * This function is run when the robot is first started up and should be
@@ -55,17 +57,32 @@ public class Robot extends IterativeRobot {
      */
     public void autonomousInit() {
     	autoLoopCounter = 0;
+    	
+    	autoArray[0] = onecommand;
+    	onecommand = new commandObject(200) {
+        	public void worker() {
+        		myRobot.drive(0.2, 0.0); 	// drive forwards half speed	
+        	};
+    	};
+    	
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
+    	for(int i = autoArray.length-1; i > 0; i--) {
+    		if(autoLoopCounter < autoArray[i].count) {
+    			autoArray[i].worker();
+    			autoLoopCounter++;
+    			break;
+    		};
+    	};
     	if(autoLoopCounter < 100) //Check if we've completed 100 loops (approximately 2 seconds)
 		{
 			myRobot.drive(0.2, 0.0); 	// drive forwards half speed
 			autoLoopCounter++;
-			} else if(autoLoopCounter < 102) {
+		} else if(autoLoopCounter < 102) {
 			myRobot.drive(0.0, 0.0); 	// stop robot
 			autoLoopCounter++;
 		} else if(autoLoopCounter < 110) {
@@ -131,11 +148,6 @@ public class Robot extends IterativeRobot {
         	}else if(leftStick.getRawButton(8)){
         		pump.stop();
         	};
-        	if(){
-        		
-        	}else if() {
-        		
-        	};
         }
     }
     
@@ -147,3 +159,5 @@ public class Robot extends IterativeRobot {
     }
     
 }
+
+
---
+++ src/org/usfirst/frc/team4924/robot/commandObject.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team4924.robot;
+
+public class commandObject{
+	public int count;
+	public commandObject(int inputCount) {
+		count = inputCount;
+	};
+	
+	public void worker() {
+		
+	};
+	
+};
+
